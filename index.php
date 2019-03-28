6851e9096a594485a51fa1b6631671ae60a7ed1e
nimaaz <43337683+nima1233@users.noreply.github.com>
2019-03-28 10:12:06 +0100

    link

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Document</title>
     <?php
 		include("Templates/nav.php")
