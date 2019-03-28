7f890e28d3160075a9753710dd32fcb158f9c33d
Ashur213 <ashur7@live.se>
2019-03-28 09:54:50 +0100

    Nav

--- templates/nav.php
+++ templates/nav.php
@@ -1,8 +1,8 @@
 <nav>
-<li>
+<ul>
                 <li><a href="index.php">register</a></li>
                 <li><a href="meny.php">meny</a></li>
 				<li><a href="boka.php">boka</a></li>
 				<li><a href="kontakt.php">kontakta oss</a></li
-</li>
+</ul>
 </nav>
