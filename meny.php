eec9ae15733322010f5e0104503a639d4760885d
nimaaz <43337683+nima1233@users.noreply.github.com>
2019-04-11 09:54:46 +0200

    Update meny.php

--- meny.php
+++ meny.php
@@ -11,17 +11,71 @@
         <div class="info_container">
             <div class="price"> <p>100:-</p></div>
             <div class="split">
-                <div class="info"><p class="name">Pad nam</p><p class="info_text"> asoid ioas oija doij oij aoij o ij oij oij oj oi j j oij oij oij oij oij iojoij oijoijoijoij oijoij oijioj oij oij oijoijoij oijoij oijoijo ij oijoijo ijoijo ijoi jo ijoijo ijoij oijoij oijo ijoij oijoij oij oij oijo ij</p></div>
+                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
                 <div class="spicy"></div>
             </div>
         </div>
     </div>
-    <div class="dish_container"></div>
-    <div class="dish_container"></div>
-    <div class="dish_container"></div>
-    <div class="dish_container"></div>
-    <div class="dish_container"></div>
-    <div class="dish_container"></div>
+    <div class="dish_container">
+        <img src="bilder/padnam.jpg" height="200px" width="40%">
+        <div class="info_container">
+            <div class="price"> <p>100:-</p></div>
+            <div class="split">
+                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
+                <div class="spicy"></div>
+            </div>
+        </div>
+    </div>
+    <div class="dish_container">
+        <img src="bilder/padnam.jpg" height="200px" width="40%">
+        <div class="info_container">
+            <div class="price"> <p>100:-</p></div>
+            <div class="split">
+                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
+                <div class="spicy"></div>
+            </div>
+        </div>
+    </div>
+    <div class="dish_container">
+        <img src="bilder/padnam.jpg" height="200px" width="40%">
+        <div class="info_container">
+            <div class="price"> <p>100:-</p></div>
+            <div class="split">
+                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
+                <div class="spicy"></div>
+            </div>
+        </div>
+    </div>
+    <div class="dish_container">
+        <img src="bilder/padnam.jpg" height="200px" width="40%">
+        <div class="info_container">
+            <div class="price"> <p>100:-</p></div>
+            <div class="split">
+                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
+                <div class="spicy"></div>
+            </div>
+        </div>
+    </div>
+    <div class="dish_container">
+        <img src="bilder/padnam.jpg" height="200px" width="40%">
+        <div class="info_container">
+            <div class="price"> <p>100:-</p></div>
+            <div class="split">
+                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
+                <div class="spicy"></div>
+            </div>
+        </div>
+    </div>
+    <div class="dish_container">
+        <img src="bilder/padnam.jpg" height="200px" width="40%">
+        <div class="info_container">
+            <div class="price"> <p>100:-</p></div>
+            <div class="split">
+                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
+                <div class="spicy"></div>
+            </div>
+        </div>
+    </div>
     
 
 </section>
