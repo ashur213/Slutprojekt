2ce6fdef3ca08008cd0d06b35aa44032922a8d59
Ashur213 <ashur7@live.se>
2019-05-02 10:22:15 +0200

    Klar med containers

--- meny.php
+++ meny.php
@@ -5,78 +5,69 @@
 ?> 
 
 <section>
+  
+    <?php
+//ansluter till databasen i mysql
+$dbc = mysqli_connect("localhost","root","","thai");
 
+
+//mysql command
+$query = "SELECT * FROM meny";
+//specialsymboler visas
+mysqli_query($dbc,"SET NAMES utf8");
+
+//frågar databasen och visar i result
+$result = mysqli_query($dbc,$query);
+
+
+//Skriv ut alla städer från $_result
+while($row = mysqli_fetch_array($result)){
+    ?>
     <div class="dish_container">
-        <img src="bilder/padnam.jpg" height="200px" width="40%">
+
+        <div class="img_container">
+            <img src=<?php echo $row["bild"]; ?>>
+        </div>
+        
         <div class="info_container">
-            <div class="price"> <p>100:-</p></div>
-            <div class="split">
-                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
-                <div class="spicy"></div>
+            <div class="name">
+                <p><?php echo $row["namn"]; ?></p>
+            </div>
+            <div class="price">
+                <p><?php echo $row["pris"]; ?>:-</p>                
+            </div>
+            <div class="desc_container">
+                <div class="desc">
+                    <p><?php echo $row["info"]; ?></p>
+                </div>
+                <div class="side_info">
+                    <div class="milk">
+                        <p><?php echo $row["mjölk"]; ?></p>                
+                    </div>
+                    <div class="gluten">
+                        <p><?php echo $row["gluten"]; ?></p>                
+                    </div>
+                    <div class="nuts">
+                        <p><?php echo $row["nötter"]; ?></p>                
+                    </div>
+                    <div class="spicy">
+                        <p><?php echo $row["styrka"]; ?></p>                
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+                
             </div>
         </div>
     </div>
-    <div class="dish_container">
-        <img src="bilder/padnam.jpg" height="200px" width="40%">
-        <div class="info_container">
-            <div class="price"> <p>100:-</p></div>
-            <div class="split">
-                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
-                <div class="spicy"></div>
-            </div>
-        </div>
-    </div>
-    <div class="dish_container">
-        <img src="bilder/padnam.jpg" height="200px" width="40%">
-        <div class="info_container">
-            <div class="price"> <p>100:-</p></div>
-            <div class="split">
-                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
-                <div class="spicy"></div>
-            </div>
-        </div>
-    </div>
-    <div class="dish_container">
-        <img src="bilder/padnam.jpg" height="200px" width="40%">
-        <div class="info_container">
-            <div class="price"> <p>100:-</p></div>
-            <div class="split">
-                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
-                <div class="spicy"></div>
-            </div>
-        </div>
-    </div>
-    <div class="dish_container">
-        <img src="bilder/padnam.jpg" height="200px" width="40%">
-        <div class="info_container">
-            <div class="price"> <p>100:-</p></div>
-            <div class="split">
-                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
-                <div class="spicy"></div>
-            </div>
-        </div>
-    </div>
-    <div class="dish_container">
-        <img src="bilder/padnam.jpg" height="200px" width="40%">
-        <div class="info_container">
-            <div class="price"> <p>100:-</p></div>
-            <div class="split">
-                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
-                <div class="spicy"></div>
-            </div>
-        </div>
-    </div>
-    <div class="dish_container">
-        <img src="bilder/padnam.jpg" height="200px" width="40%">
-        <div class="info_container">
-            <div class="price"> <p>100:-</p></div>
-            <div class="split">
-                <div class="info"><p class="name">Pad nam</p><p class="info_text"> JÄTTEGOD PADNAM PADNAM PADNAMPADNAMPADNAMPADNAMPADNAMPADNAMPADNAM</p></div>
-                <div class="spicy"></div>
-            </div>
-        </div>
-    </div>
-    
+    <?php
+}
+?>
+    ?>
+?>
+
 
 </section>
 
